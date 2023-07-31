c5e477c1ad596d3195a6a4827f315b63afda7203
mayks1 <mayks1@gmail.com>
2023-07-31 20:46:53 +0300

    Move CSS File

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/assets/images')
     eleventyConfig.addPassthroughCopy('./src/assets/blog')
-    eleventyConfig.addPassthroughCopy('./node_modules/@picocss/pico/css/pico.css')
+    eleventyConfig.addPassthroughCopy('./src/css/pico.css')
     // eleventyConfig.addCollection('post', function(collectionApi) {
     //     return collectionApi.getFilteredByGlob('src/articles/**/*.md')
     // })
