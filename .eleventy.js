7886ba6ccf11a4a9049a6c52fac5d3936608cca8
mayks1 <mayks1@gmail.com>
2023-08-20 12:33:02 +0300

    Added New robots.njk

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/fonts')
     eleventyConfig.addPassthroughCopy("./src/favicon.png")
     // Put robots.txt in root
-    eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' })
+    // eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' })
 
     eleventyConfig.addCollection('publishedPosts', (collectionApi) => {
         let posts = collectionApi
